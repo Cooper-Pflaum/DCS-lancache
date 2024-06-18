f07f7e8b58afc2ce0d066bd014c0898b4f6911b4
Cooper Pflaum <cooperpflaum@gmail.com>
2024-06-18 12:58:26 -0400

    Updating read prompt

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ echo -e "--------------------------------- DNS IP ------------------------------
 echo -e "  Please provide the DNS IPs that you use\n"
 echo -e "  If you wish to provide multiple they need to be seperated by a space:\n"
 echo -e "    (Ex) DNS IP Address: \e[1;34m8.8.8.8 8.8.4.4\n\e[0m"
-read -p "DNS IP Address: \e[34m" dns_addr 
+read -p $"DNS IP Address: \e[34m" dns_addr 
 
 echo -e "\n\e[0m"
 
@@ -35,7 +35,7 @@ echo -e "  All other connections to the server will be denied\n"
 echo -e "  Note: This IP that you select should be able to successfully ping this server\n"
 echo -e "  \e[1;31mWARNING:\e[0m if you set this to \e[1;31mall\e[0m, any person on the same LAN can monitor these servers\e[0m\n\n"
 echo -e "    (Ex) Monitor IP Address: \e[1;34m127.0.0.1\n\e[0m"
-read -p "Monitor IP Address: \e[34m" mon_addr
+read -p $"Monitor IP Address: \e[34m" mon_addr
 
 echo -e "\n\e[0m"
 
